4ec70b567af40e4fedaff5ed40d7d3e7e8052d2a
LePhuThanh <lpthanh710@gmail.com>
2023-10-05 16:36:17 +0700

    full

--- src/main/java/com/lpt/spring_jms/util/EmailUtil.java
+++ src/main/java/com/lpt/spring_jms/util/EmailUtil.java
@@ -11,16 +11,20 @@ import org.springframework.stereotype.Component;
 public class EmailUtil {
     @Autowired
     private JavaMailSender javaMailSender;
+
     public void sendOtpEmail(String email, String otp) throws MessagingException {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
         mimeMessageHelper.setTo(email);
         mimeMessageHelper.setSubject("Verify OTP");
-        mimeMessageHelper.setText("""
+        mimeMessageHelper.setText(
+                """
                         <div>
                             <a href = "http://localhost:8080/verify-account?email=%s&otp=%s" target = "_blank">Click link to verify </a>
                         </div>
-                        """.formatted(email, otp),true);
+                        """
+                        .formatted(email, otp),
+                true);
         javaMailSender.send(mimeMessage);
     }
 
@@ -29,11 +33,14 @@ public class EmailUtil {
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
         mimeMessageHelper.setTo(email);
         mimeMessageHelper.setSubject("Set Password");
-        mimeMessageHelper.setText("""
+        mimeMessageHelper.setText(
+                """
                         <div>
                             <a href = "http://localhost:8080/set-password?email=%s" target = "_blank">Click link to set password </a>
                         </div>
-                        """.formatted(email),true);
+                        """
+                        .formatted(email),
+                true);
         javaMailSender.send(mimeMessage);
     }
 }
